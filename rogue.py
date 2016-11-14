f30913a0ee03012c599af77c6c6ffb0496180abb
Caleb Bernard <calebbernard@gmail.com>
2016-11-14 16:04:12 -0600

    item gen first draft

--- rogue.py
+++ rogue.py
@@ -16,6 +16,21 @@ MAXPACK = 23
 MAXTRAPS = 10
 NUMTHINGS = 7
 
+BEARTIME = 3
+SLEEPTIME = 5
+HEALTIME = 30
+HOLDTIME = 2
+STPOS = 0
+WANDERTIME = 70
+BEFORE = 1
+AFTER = 2
+HUHDURATION = 20
+SEEDURATION = 850
+HUNGERTIME = 1300
+MORETIME = 150
+STOMACHSIZE = 2000
+BOLT_LENGTH = 6
+
 
 flags = {'ISDARK':0000001,'ISCURSED':0000001,'ISBLIND':0000001,
 	'ISGONE':0000002,'ISKNOW':0000002,'ISRUN':0000004,'ISFOUND':0000010,
@@ -28,6 +43,73 @@ tiles = {'PASSAGE':'#','DOOR':'+','FLOOR':'.','PLAYER':'@','TRAP':'^','TRAPDOOR'
 	'ARROWTRAP':'{','SLEEPTRAP':'$','BEARTRAP':'}','TELTRAP':'~','DARTTRAP':'`',
 	'SECRETDOOR':'&','STAIRS':'%','GOLD':'*','POTION':'!','SCROLL':'?','MAGIC':'$',
 	'FOOD':':','WEAPON':')','ARMOR':']','AMULET':',','RING':'=','STICK':'/'}
+
+saves = {'VS_POISON':00,'VS_PARALYZATION':00,'VS_DEATH':00,'VS_PETRIFICATION':01,
+	'VS_BREATH':02,'VS_MAGIC':03}
+
+w_dam = ["2d4", "1d10", "1d1", "1d1", "1d6", "1d2", "3d6", "0d0", "1d1", "1d2", "1d8"]
+
+w_hrl = ["1d3", "1d2", "1d1", "1d6", "1d4", "1d4", "1d2", "0d0", "1d3", "1d1", "1d10", "1d6"]
+
+w_launch = ["NONE", "NONE", "NONE", "BOW", "NONE", "SLING", "NONE", "NONE", "NONE", "NONE", "CROSSBOW", "NONE"]
+
+w_flags = [0,0,0,flags['ISMANY']|flags['ISMISL'],flags['ISMISL'],flags['ISMANY']|flags['ISMISL'],0,0,flags['ISMANY']|flags['ISMISL'],0,flags['ISMANY']|flags['ISMISL'],flags['ISMISL']]
+
+# Not needed?
+item_types = {'POTION':0,'SCROLL':1,'FOOD':2,'WEAPON':3,'ARMOR':4,'RING':5,'STICK':6}
+
+potion_types = {'P_CONFUSE':0,'P_PARALYZE':1,'P_POISON':2,'P_STRENGTH':3,'P_SEEINVIS':4,
+	'P_HEALING':5,'P_MFIND':6,'P_TFIND':7,'P_RAISE':8,'P_XHEAL':9,'P_HASTE':10,
+	'P_RESTORE':11,'P_BLIND':12,'P_NOP':13}
+
+scroll_types = {'S_CONFUSE':0,'S_MAP':1,'S_LIGHT':2,'S_HOLD':3,'S_SLEEP':4,'S_ARMOR':5,
+	'S_IDENT':6,'S_SCARE':7,'S_GFIND':8,'S_TELEP':9,'S_ENCH':10,'S_CREATE':11,
+	'S_REMOVE':12,'S_AGGR':13,'S_NOP':14,'S_GENOCIDE':15}
+
+weapon_types = {'MACE':0,'SWORD':1,'BOW':2,'ARROW':3,'DAGGER':4,'ROCK':5,'TWOSWORD':6,
+	'SLING':7,'DART':8,'CROSSBOW':9,'BOLT':10,'SPEAR':11}
+
+armor_types = {'LEATHER':0,'RING_MAIL':1,'STUDDED_LEATHER':2,'SCALE_MAIL':3,'CHAIN_MAIL':4,
+	'SPLINT_MAIL':5,'BANDED_MAIL':6,'PLATE_MAIL':7}
+
+ring_types = {'R_PROTECT':0,'R_ADDSTR':1,'R_SUSTSTR':2,'R_SEARCH':3,'R_SEEINVIS':4,
+	'R_NOP':5,'R_AGGR':6,'R_ADDHIT':7,'R_ADDDAM':8,'R_REGEN':9,'R_DIGEST':10,
+	'R_TELEPORT':11,'R_STEALTH':12}
+
+stick_types = {'WS_LIGHT':0,'WS_HIT':1,'WS_ELECT':2,'WS_FIRE':3,'WS_COLD':4,
+	'WS_POLYMORPH':5,'WS_MISSILE':6,'WS_HASTE_M':7,'WS_SLOW_M':8,'WS_DRAIN':0,
+	'WS_NOP':10,'WS_TELAWAY':11,'WS_TELTO':12,'WS_CANCEL':13}
+
+s_magic = [["monster confusion",8,170],["magic mapping",5,180],["light",5,180],
+	["hold monster",2,200],["sleep",5,50],["enchant armor",8,130],["identify",21,100],
+	["scare monster",4,180],["gold detection",4,110],["teleportation",7,175],
+	["enchant weapon",10,150],["create monster",5,75],["remove curse",8,105],
+	["aggravate monsters",1,60],["blank paper",1,50],["genocide",1,200]]
+
+p_magic = [["confusion",8,50],["paralysis",10,50],["poison",8,50],["gain strength",15,150],
+	["see invisible",2,170],["healing",15,130],["monster detection",6,120],
+	["magic detection",6,105],["raise level",2,220],["extra healing",5,180],
+	["haste self",4,200],["restore strength",14,120],["blindness",4,50],
+	["thirst quenching",1,50]]
+
+r_magic = [["protection",9,200],["add strength",9,200],["sustain strength",5,180],
+	["searching",10,200],["see invisible",10,175],["adornment",1,100],
+	["aggravate monster",11,100],["dexterity",8,220],["increase damage",8,220],
+	["regeneration",4,260],["slow digestion",9,240],["teleportation",9,100],
+	["stealth",7,100]]
+
+ws_magic = [["light",12,120],["striking",9,115],["lightning",3,200],["fire",3,200],
+	["cold",3,200],["polymorph",15,210],["magic missile",10,170],["haste monster",9,50],
+	["slow monster",11,220],["drain life",9,210],["nothing",1,70],["teleport away",5,140],
+	["teleport to",5,60],["cancellation",5,130]]
+
+a_class = [8,7,7,6,5,4,4,3]
+
+a_names = ["leather armor", "ring mail", "studded leather armor", "scale mail",
+	"chain mail", "splint mail", "banded mail", "plate mail"]
+
+a_chances = [20,35,50,63,75,85,95,100]
+
 
 # vars
 amulet = 0
@@ -395,23 +477,126 @@ def do_passages():
 			r1.isconn[j] = 1
 			r2.isconn[i] = 1
 
+def pick_one(magic_list):
+	for x in magic_list:
+		i = rnd(100)
+		if i < x[2]:
+			return x[0]
+	return magic_list[0][0]
+
+
+def new_thing():
+	global group, no_food
+	empty = coord(0,0)
+	item = object(0,empty,0,0,0,0,0,0,0,0,0,0)
+	item.o_hplus = item.o_dplus = 0
+	item.o_damage = item.o_hurldmg = "0d0"
+	item.o_ac = 11
+	item.o_count = 1
+	item.o_group = 0
+	item.o_flags = 0
+	if no_food > 3:
+		thing = 2
+	else:
+		thing = rnd(NUMTHINGS)
+	if thing == 0:
+		item.o_type = 'POTION'
+		item.o_which = pick_one(p_magic)
+	elif thing == 1:
+		item.o_type = 'SCROLL'
+		item.o_which = pick_one(s_magic)
+	elif thing == 2:
+		no_food = 0
+		item.o_type = 'FOOD'
+		if rnd(100) > 10:
+			item.o_which = 0
+		else:
+			item.o_which = 1
+	elif thing == 3:
+		item.o_type = 'WEAPON'
+		item.o_which = rnd(len(weapon_types))
+		item.o_damage = w_dam[item.o_which]
+		item.o_hurldmg = w_hrl[item.o_which]
+		item.o_launch = w_launch[item.o_which]
+		item.o_flags = w_flags[item.o_which]
+		if item.o_flags & flags['ISMANY']:
+			item.o_count = rnd(8) + 8
+			item.o_group = group
+			group += 1
+		else:
+			item.o_count = 1
+			k = rnd(100)
+			if k < 10:
+				item.o_flags |= flags['ISCURSED']
+				item.o_hplus -= rnd(3)+1
+	elif thing == 4:
+		item.o_type = 'ARMOR'
+		for j in range(len(armor_types)):
+			k = rnd(100)
+			if k < a_chances[j]:
+				break
+			if j == len(armor_types):
+				j == 0
+			item.o_which = j
+			item.o_ac = a_class[j]
+			k = rnd(100)
+			if k < 20:
+				item.o_flags |= flags['ISCURSED']
+				item.o_ac += rnd(3) + 1
+			elif k < 28:
+				item.o_ac -= rnd(3) + 1
+	elif thing == 5:
+		item.o_type = 'RING'
+		item.o_which = pick_one(r_magic)
+		if item.o_which == ring_types['R_ADDSTR'] or item.o_which == ring_types['R_PROTECT'] or	item.o_which == ring_types['R_ADDHIT'] or item.o_which == ring_types['R_ADDDAM']:
+				item.o_ac = rnd(3)
+				if item.o_ac == 0:
+					item.o_ac = -1
+					item.o_ac |= flags['ISCURSED']
+		if item.o_which == ring_types['R_AGGR'] or item.o_which == ring_types['R_TELEPORT']:
+				item.o_flags |= flags['ISCURSED']
+	elif thing == 6:
+		item.o_type = 'STICK'
+		item.o_which = pick_one(ws_magic)
+		#No extra staff damage, for simplicity's sake
+		item.o_damage = "1d1"
+		item.o_hurldmg = "1d1"
+		item.o_charges =rnd(5) + 3
+		if item.o_which == stick_types['WS_HIT']:
+			item.o_hplus = 3
+			item.o_dplus = 3
+			item.o_damage = "1d8"
+		elif item.o_which == stick_types['WS_LIGHT']:
+			item.o_charges = rnd(10) + 10
+	return item
 
 
 def new_level():
 	stdscr.erase()
-	global level, max_level, no_food, player
+	global level, max_level, no_food, player, items, lvl_obj
 	if level > max_level:
 		max_level = level
 	do_rooms()
 	do_passages()
 	no_food += 1
-	# Put_things()
-	# Add stairs
-	rm = rnd_room()
-	stairs = rooms[rm].r_gold
-	while stairs == rooms[rm].r_gold:
-		stairs = rnd_pos(rooms[rm])
-	stdscr.addch(stairs.y, stairs.x, tiles['STAIRS'])
+
+	# Add items
+	if amulet == 1 or level >= max_level:
+		for x in range(MAXOBJ):
+			if rnd(100) < 35:
+				item = new_thing()
+				lvl_obj.append(item)
+				rm = rnd_room()
+				done = 0
+				while done == 0:
+					item.o_pos = rnd_pos(rooms[rm])
+					tile = chr(stdscr.inch(item.o_pos.y, item.o_pos.x) & 0x00FF)
+					if tile == tiles['FLOOR']:
+						stdscr.addch(tiles[item.o_type])
+						done = 1
+	#put amulet generation here
+
+
 	# Add traps
 	if (rnd(10) < level):
 		ntraps = rnd(level/4) + 1
@@ -439,7 +624,17 @@ def new_level():
 			traps[i].tr_type = ch
 			traps[i].tr_flags = 0
 			traps[i].tr_pos = trappos
+
+	# Add stairs
+	rm = rnd_room()
+	stairs = rooms[rm].r_gold
+	while stairs == rooms[rm].r_gold:
+		stairs = rnd_pos(rooms[rm])
+	stdscr.addch(stairs.y, stairs.x, tiles['STAIRS'])
+
 	# Add light
+
+	# Add player
 	rm = rnd_room()
 	playerpos = rnd_pos(rooms[rm])
 	oldch = chr(stdscr.inch(playerpos.y, playerpos.x) & 0x00FF)
@@ -448,7 +643,7 @@ def new_level():
 	player.t_pos = playerpos
 
 def init():
-	global MAXROOMS, rooms, level, no_food, traps, player, max_hp, max_stats, purse
+	global MAXROOMS, group, rooms, level, no_food, traps, player, max_hp, max_stats, purse, pack, lvl_obj
 	empty=coord(0,0)
 	empty_str_t = str_t(0,0)
 	emptystats=stats(empty_str_t,0,0,0,0,"")
@@ -470,19 +665,19 @@ def init():
 	max_stats = player.t_stats
 	item = object(0,empty,0,"","",0,0,0,0,0,0,0)
 	purse = 0
-
+	lvl_obj = []
+	group = 0
 	level = 1
 	random.seed()
 	no_food = 0
 	new_level()
 
 def status():
-	global level, purse
-	hp = 12
-	maxhp = 12
-	strength = 16
+	global level, purse, max_hp
+	hp = player.t_stats.s_hpt
+	strength = player.t_stats.s_str.st_str
 	status = "Level: " + str(level) + " Gold: " + str(purse) + " Hp: "
-	status +=  str(hp) + "(" + str(maxhp) + ") Str: " + str(strength)
+	status +=  str(hp) + "(" + str(max_hp) + ") Str: " + str(strength)
 	stdscr.addstr(LINES - 1, 0, status)
 
 def do_move(dy,dx):
@@ -527,13 +722,13 @@ def command():
 		do_move(1,1)
 
 	# Pick up stuff
+	for x in rooms:
+		if x.r_gold.x == player.t_pos.x and x.r_gold.y == player.t_pos.y:
+			purse += x.r_goldval
+			x.r_gold.x = x.r_gold.y = 0
+			player.t_oldch = tiles['FLOOR']
 
-	if take == "money":
-		for x in rooms:
-			if x.r_gold.x == player.t_pos.x and x.r_gold.y == player.t_pos.y:
-				purse += x.r_goldval
-				x.r_gold.x = x.r_gold.y = 0
-				player.t_oldch = tiles['FLOOR']
+
 
 	if ch == ord('q'):
 		sys.exit()
